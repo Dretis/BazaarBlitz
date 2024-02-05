20c8b436758135db8b85701dfb366a055da87e7a
LeUmbono <theumbono@gmail.com>
2024-02-05 13:16:01 -0800

    fixed small bug with encounters

--- Assets/Scripts/SceneGameManager.cs
+++ Assets/Scripts/SceneGameManager.cs
@@ -30,6 +30,7 @@ public class SceneGameManager : MonoBehaviour
     public void UnloadCombatScene()
     {
         SceneManager.UnloadSceneAsync("CombatTest");
+        overworldScene.encounterStarted = false;
     }
 
     public void ChangeGamePhase(GameplayTest.GamePhase phase)
