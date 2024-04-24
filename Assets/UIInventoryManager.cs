939d648f93211f1a0a8b7bb3e8864199b7c70165
Xerial <xerialbole@gmail.com>
2024-04-24 03:29:58 -0700

    More Player Inventory UI updates

--- Assets/UIInventoryManager.cs
+++ Assets/UIInventoryManager.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
+using static UnityEditor.Progress;
 
 public class UIInventoryManager : MonoBehaviour
 {
-    [Header("UI Elements")]
-    [SerializeField] private List<TextMeshProUGUI> itemNames;
+    [SerializeField] private List<ItemStats> playerInventory;
+
+    [Header("Main Inventory")]
     [SerializeField] private Canvas inventoryCanvas;
+    [SerializeField] private CanvasGroup inventoryGroup;
+    [SerializeField] private List<Image> itemIcons;
+    [SerializeField] private List<TextMeshProUGUI> itemNames;
+    [SerializeField] private List<TextMeshProUGUI> itemPrices;
+
+    [Header("Selected Item Details")]
+    [SerializeField] private Image selectedItemIcon;
+    [SerializeField] private List<TextMeshProUGUI> selectedItemInfo;
 
     [Header("Broadcast on Event Channels")]
     public IntEventChannelSO m_ItemUsed;
@@ -19,6 +30,7 @@ public class UIInventoryManager : MonoBehaviour
 
     private void OnEnable()
     {
+        inventoryGroup.alpha = 0;
         m_OpenInventory.OnEventRaised += DisplayInventory;
         m_ExitInventory.OnEventRaised += HideInventory;
     }
@@ -31,26 +43,42 @@ public class UIInventoryManager : MonoBehaviour
 
     private void DisplayInventory(EntityPiece entity)
     {
-        inventoryCanvas.enabled = true;
+        DOTween.To(() => inventoryGroup.alpha, x => inventoryGroup.alpha = x, 1, 0.25f);
 
-        var playerInv = entity.inventory;
+        //inventoryCanvas.enabled = true;
+        HideSelectedItemDetails();
+
+        playerInventory.Clear();
+        foreach (ItemStats item in entity.inventory)
+        {
+            playerInventory.Add(item);
+        }
+
         ItemStats playerItem = null;
 
         for (int i = 0; i < itemNames.Count; i++)
         {
-            if(i < playerInv.Count)
+            if(i < playerInventory.Count)
             {
-                playerItem = playerInv[i];
+                playerItem = playerInventory[i];
             }
 
-            if (playerItem == null || i >= playerInv.Count)
+            if (playerItem == null || i >= playerInventory.Count)
             {
+                itemIcons[i].sprite = null;
+                itemIcons[i].enabled = false;
                 itemNames[i].text = "";
+                itemPrices[i].text = "";
+
                 itemNames[i].GetComponentInParent<Button>().interactable = false;
             }
             else
             {
-                itemNames[i].text = $"{playerItem.itemName} <size=16>{playerItem.effectDescription}</size>";
+                itemIcons[i].sprite = playerItem.itemSprite;
+                itemIcons[i].enabled = true;
+                itemNames[i].text = $"{playerItem.itemName}";
+                itemPrices[i].text = $"<color=#C3B789>@</color>{playerItem.basePrice}";
+
                 itemNames[i].GetComponentInParent<Button>().interactable = true;
             }
         }
@@ -58,7 +86,8 @@ public class UIInventoryManager : MonoBehaviour
 
     private void HideInventory()
     {
-        inventoryCanvas.enabled = false;
+        DOTween.To(() => inventoryGroup.alpha, x => inventoryGroup.alpha = x, 0, 0.25f);
+        //inventoryCanvas.enabled = false;
     }
 
     public void UseItem(int index)
@@ -66,14 +95,44 @@ public class UIInventoryManager : MonoBehaviour
         // Tell listeners that the item at this index in player's inventory is being used
         m_ItemUsed.RaiseEvent(index);
 
+        playerInventory[index] = null;
+
         if (!GameplayTest.instance.isStockingStore)
         {
+            itemIcons[index].sprite = null;
+            itemIcons[index].enabled = false;
             itemNames[index].text = "";
+            itemPrices[index].text = "";
             for (int i = 0; i < itemNames.Count; i++)
             {
                 // Make the rest of the inventory interactable, prevents multiple item uses in one turn
                 itemNames[i].GetComponentInParent<Button>().interactable = false;
             }
         }
+    }
+
+    public void ShowSelectedItemDetails(int index)
+    {
+        if (index >= playerInventory.Count || playerInventory[index] == null)
+            return;
+
+        selectedItemIcon.sprite = playerInventory[index].itemSprite;
+        selectedItemIcon.enabled = true;
+
+        selectedItemInfo[0].text = $"{playerInventory[index].itemName}";
+        selectedItemInfo[1].text = $"<color=#C3B789>@</color>{playerInventory[index].basePrice}";
+        selectedItemInfo[2].text = $"{playerInventory[index].effectDescription}";
+        selectedItemInfo[3].text = $"{playerInventory[index].flavorText}";
+    }
+
+    public void HideSelectedItemDetails()
+    {
+        selectedItemIcon.sprite = null;
+        selectedItemIcon.enabled = false;
+
+        selectedItemInfo[0].text = "";
+        selectedItemInfo[1].text = "";
+        selectedItemInfo[2].text = "";
+        selectedItemInfo[3].text = "";
     }
 }
