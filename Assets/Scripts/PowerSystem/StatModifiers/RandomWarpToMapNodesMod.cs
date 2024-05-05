17448a1141127089fc39016650032025860afb6a
LeUmbono <theumbono@gmail.com>
2024-05-05 15:12:32 -0700

    added warp node testing

--- Assets/Scripts/PowerSystem/StatModifiers/RandomWarpToMapNodesMod.cs
+++ Assets/Scripts/PowerSystem/StatModifiers/RandomWarpToMapNodesMod.cs
@@ -1,13 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "StatMods/RandWarpMod")]
-public class RandomWarpMod : StatModifierChangerSO
+[CreateAssetMenu(menuName = "StatMods/RandWarpToMapNodesMod")]
+public class RandomWarpToMapNodesMod : StatModifierChangerSO
 {
     public List<MapNode> nodes;
 
     public override EntityStatsModifiers ApplyStatModChanges(EntityStatsModifiers currentStats, int currentTurn)
     {
+        // Try this for now, might change to be more efficient later.
+        List<MapNode> nodes = new List<MapNode>(GameObject.FindObjectsOfType<MapNode>());
         currentStats.warpDestination = nodes[Random.Range(0, nodes.Count)];
 
         return currentStats;
