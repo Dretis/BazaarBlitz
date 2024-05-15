8b6c756b09fc2b0a3a020ad7f30fe594bf1b3e0d
LeUmbono <theumbono@gmail.com>
2024-05-14 20:04:48 -0700

    test combat items

--- Assets/Scripts/ItemLists.cs
+++ Assets/Scripts/ItemLists.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class ItemLists
 {
     public static HashSet<string> StealOnPassByItemNames = new HashSet<string> { "TestSteal" };
-    public static HashSet<string> CombatOnPassByItemNames = new HashSet<string> { "Rice Wine", "Cobra Whiskey" };
+    public static HashSet<string> CombatOnPassByItemNames = new HashSet<string> { "TestCombat", "Rice Wine", "Cobra Whiskey" };
 }
