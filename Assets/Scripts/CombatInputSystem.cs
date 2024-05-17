75110ca8eb618bd4a9ab8ff7d81c65f949de8022
James Jasper Fadden O'Roarke <faddenjasper@gmail.com>
2024-05-17 11:39:06 -0700

    Making finishing touches to first revision of combat script

--- Assets/Scripts/CombatInputSystem.cs
+++ Assets/Scripts/CombatInputSystem.cs
@@ -3,7 +3,33 @@ using UnityEngine;
 public class CombatInputSystem : MonoBehaviour
 {
     public Canvas howToPlayScreen;
+<<<<<<< Updated upstream
     // Update is called once per frame
+=======
+
+    // Seems the on press stuff is something I'll have to do in person :(
+    // I tried to make the update code self contained so it shouldn't be a hard port.
+    bool player1Went = false;
+    bool player2Went = false;
+
+    bool player1Attacking;
+    bool player2Attacking;
+
+    EntityPiece player1;
+    EntityPiece player2;
+
+    //public ????? m_ActionSelected;
+
+    public PlayerEventChannelSO m_SwapPhase; // Will be used to notify combatinputsystem when a turn is finished and new input is needed
+
+    void OnEnable() {
+        
+
+
+        m_SwapPhase.OnEventRaised += PhasePassed;
+    }
+
+>>>>>>> Stashed changes
     void Update()
     {
         bool currentPlayer = CombatManager.Instance.isInitiatorTurn;
@@ -32,4 +58,69 @@ public class CombatInputSystem : MonoBehaviour
             howToPlayScreen.enabled = false;
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    void sendAction(bool isPlayer1, int actionID) {
+        player1Attacking = CombatManager.Instance.player1Attacking;
+        player2Attacking = CombatManager.Instance.player2Attacking;
+
+        player1 = CombatManager.Instance.player1;
+        player2 = CombatManager.Instance.player2;
+
+        if (player1Went && isPlayer1) { // Players who already went have to commit
+            return;
+        } else if (player2Went && !isPlayer1) {
+            return;
+        } else if (!isPlayer1 && player2.isEnemy) { // No manual enemy control
+            return;
+        }
+
+        Debug.Log(actionID);
+
+        if (isPlayer1) {
+            player1Went = true;
+            Action action;
+            if (player1Attacking) {
+                action = player1.attackActions[actionID];
+            }
+            else {
+                action = player1.defendActions[actionID];
+            }
+            CombatManager.Instance.ActionSelected(player1, action);
+            //m_ActionSelected.RaiseEvent(player1, action);
+        } else {
+            player2Went = true;
+            Action action;
+            if (player2Attacking) {
+                action = player2.attackActions[actionID];
+            }
+            else {
+                action = player2.defendActions[actionID];
+            }
+            CombatManager.Instance.ActionSelected(player2, action);
+            //m_ActionSelected.RaiseEvent(player2, action);
+        }
+    }
+
+    private void PhasePassed(EntityPiece attacker) {
+        player1Went = false;
+        player2Went = false;
+
+        if (player1Attacking) {
+            player1Attacking = false;
+        } else {
+            player1Attacking = true;
+        }
+
+        if (player2Attacking) {
+            player2Attacking = false;
+        } else {
+            player2Attacking = true;
+        }
+        
+    }
+
+
+>>>>>>> Stashed changes
 }
--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -62,7 +62,7 @@ public class CombatManager : MonoBehaviour
     public PlayerEventChannelSO m_SwapPhase; // void event
 
     public EntityActionPhaseEventChannelSO m_ActionSelected; // Entity, check side and phase | Either the attacker or defender picked an action
-    //public ActionSelectEventChannelSO m_BothActionsSelected; // prep time to show what they picked, follow with the dice roll too
+    public EntityActionEventChannelSO m_BothActionsSelected; // both players call this at once to start their animation showing their items
     public DamageEventChannelSO m_DiceRolled; // 2 floats
 
     public PlayerEventChannelSO m_PlayOutCombat; // play attack anim and defend anim
@@ -72,6 +72,15 @@ public class CombatManager : MonoBehaviour
     public EntityItemEventChannelSO m_EntityDied; // someone's HP dropped to 0, Victory, show rewards
     public VoidEventChannelSO m_Stalemate; // Combat is suspended, no one died this time
 
+<<<<<<< Updated upstream
+=======
+    //public EntityActionEventChannelSO m_ActionSelected; // I'm leaving this part till after the tuesday meeting, as it should use the same input system as the controller.
+    // For now, a debug implementation with WASD and arrowkeys is in place that I'll soon replace. (I also had 1 controller so I couldn't debug at home)
+
+
+
+    // Code from the old combat manager to set things up. Russell wrote most of this so I mostly copied over in the revamp, with slight edits.
+>>>>>>> Stashed changes
     private void Awake()
     {
         sceneManager = GameObject.FindWithTag("SceneManager").GetComponent<SceneGameManager>();
@@ -229,6 +238,7 @@ public class CombatManager : MonoBehaviour
             m_ActionSelected.RaiseEvent(attacker, Action.PhaseTypes.Attack);
         }
 
+<<<<<<< Updated upstream
         // On turn 2 against a now attacking enemy, immediately decide its attack action.
         if ((!isInitiatorTurn && !initiatorAttacking) && isFightingAI)
         {
@@ -274,6 +284,37 @@ public class CombatManager : MonoBehaviour
             // Return in 1 second from Update.
             pausingCombat = true;
             endCombatSceneTimer = 1.5f;
+=======
+        // The input event stuff isn't in yet (I have to talk about that during capstone), but with a tweak to combatinput and uncommenting this it should be easy
+        //m_ActionSelected.OnEventRaised += playerSelected;
+
+        waitingForSelection = true; // Now we'll continue from the event listener.
+
+        
+
+        
+    }
+
+    public void randomlyDecidePlayers(int firstPlayer) {
+        if (firstPlayer == 0) {
+            attacker = player1;
+            defender = player2;
+            player1Attacking = true;
+            player2Attacking = false;
+        } else {
+            defender = player1;
+            attacker = player2;
+            player1Attacking = false;
+            player2Attacking = true;
+        }
+    }
+
+    // Called when m_ActionSelected is raised with 
+    public void ActionSelected(EntityPiece player, Action action) {
+        sceneManager.EnableScene(combatSceneIndex);
+        if (waitingForSelection == false) {
+            Debug.Log("Turn in progress");
+>>>>>>> Stashed changes
             return;
         }
 
@@ -305,10 +346,28 @@ public class CombatManager : MonoBehaviour
             // We'll come back later from update with endingCombat = true
         }
 
+<<<<<<< Updated upstream
 
         if (initiatorWon)
         {
             Debug.Log("Initiator Wins!");
+=======
+        if (isFightingAI && action.phase == Action.PhaseTypes.Attack) {
+            defenderAction = decideAttackAI();
+            m_ActionSelected.RaiseEvent(defender, Action.PhaseTypes.Attack);
+            onePlayerSelected = true;
+        } else if (isFightingAI && action.phase == Action.PhaseTypes.Defend) {
+            attackerAction = decideAttackAI();
+            m_ActionSelected.RaiseEvent(attacker, Action.PhaseTypes.Attack);
+            onePlayerSelected = true;
+        }
+
+        // float animationLength = action.animationLength;
+
+        
+
+        StartCoroutine(SelectionAnimation(1.0f));
+>>>>>>> Stashed changes
 
             loser = retaliator;
 
@@ -456,11 +515,19 @@ public class CombatManager : MonoBehaviour
         }
         */
 
+<<<<<<< Updated upstream
         int damage = 0;
 
         // 0= Unused, 1= Gun, 2= Melee, 3=Magic. 
         Action attack = attacker.attackActions[attackerAction-1];
         Action defend = defender.defendActions[defenderAction-1];
+=======
+        m_BothActionsSelected.RaiseEvent(attacker, attackerAction); // Call animation players to run through event
+        m_BothActionsSelected.RaiseEvent(defender, defenderAction); // Call animation players to run through event
+
+    
+        // float animationLength = some constant probably;
+>>>>>>> Stashed changes
 
         
         // Melee beats magic, Magic beats gun, Gun beats melee. 1x, 1.5x, or 2x damage.
@@ -624,11 +691,80 @@ public class CombatManager : MonoBehaviour
         {
             defenderAction = ActionID;
         }
+<<<<<<< Updated upstream
+=======
+
+        damage = damage * 10;
+
+        int finalDamage = (int)(damage * (1 - (0.1f * defenseScore)) * damageTypeMultiplier);
+
+        if (finalDamage < 0)
+        {
+            finalDamage = 0;
+        }
+
+        m_PlayOutCombat.RaiseEvent(attacker); // Set attack animation to play early
+
+        //float animationLength = attack.animationLength;
+
+        StartCoroutine(AttackAndDefendAnimation(1.0f, finalDamage));
+
+    }
+
+    // Final parts of the turn here, where we actually deal damage and soon after flip phases
+    private void DealDamage(int damageToDeal) {
+        // damage was set earlier in combat calulations.
+
+        Debug.Log("Final Damage: " + damageToDeal);
+
+        defender.health -= (int)damageToDeal; // Apply final damage
+
+        attacker.health += (int)(damageToDeal * attacker.currentStatsModifier.lifestealMult); // Attacker heals if they have lifesteal
+
+        m_DamageTaken.RaiseEvent(defender, damageToDeal);
+
+        StartCoroutine(PhaseEndDelay(0.5f));
+    }
+
+    private void endPhase() {
+        waitingForSelection = true;
+        if (player1.health <= 0 || player2.health <= 0) // before we even swap, see if someone died.
+        {
+            StartCoroutine(EndCombatDelay(1.5f)); // These aren't based on animations so they're constants.
+            return;
+        }
+
+        // Now we should swap around the attacker and defender.
+        EntityPiece tempDefender = defender;
+        defender = attacker;
+        attacker = tempDefender;
+
+        combatUIManager.UpdateActionText(attacker, Action.PhaseTypes.Attack);
+        combatUIManager.UpdateActionText(defender, Action.PhaseTypes.Defend);
+
+        m_SwapPhase.RaiseEvent(attacker);
+
+
+
+        // With that done, iterate isFirstPhase to see if we should pause combat.
+        if (isFirstPhase == true) { // iterate to next phase
+            isFirstPhase = false;
+        } else { // but pause combat if we looped twice
+            isFirstPhase = true;
+            StartCoroutine(PauseCombatDelay(1f));
+        }
+        
+
+        
+        
+
+>>>>>>> Stashed changes
     }
 
 
     private int decideAttackAI()
     {
+<<<<<<< Updated upstream
         int roll = Random.Range(0, 6); // 0 - 5
         
         if (roll < 3) // total 4/6 chance of using favorite attack
@@ -647,6 +783,21 @@ public class CombatManager : MonoBehaviour
         {
             return 3;
         }
+=======
+        m_Stalemate.RaiseEvent();
+
+        // Mostly scene management stuff here
+
+        // Pause combat scene and re-enable overworld scene
+        // This does not remove the scene but makes all the game objects under the combat scene inactive.
+        // Similarly, all game objects in the overworld scene are re-enabled.
+        
+        sceneManager.ChangeGamePhase(GameplayTest.GamePhase.EndTurn);
+        sceneManager.DisableScene(combatSceneIndex);
+        sceneManager.EnableScene(0);
+
+        // This changes the game phase in gameplay test remotely.
+>>>>>>> Stashed changes
     }
 
     private int decideDefendAI()
@@ -672,7 +823,78 @@ public class CombatManager : MonoBehaviour
                 return decideAttackAI();
             }
         }
+<<<<<<< Updated upstream
         else if (roll < 4)
+=======
+    
+        // Rest is (mostly) scene management stuff / events
+
+        // Players exit combat. A combatSceneIndex of -1 indicates they are out of combat. Otherwise, the scene index
+        //  variable takes the current sceneIndex of the scene.
+        player1.combatSceneIndex = -1;
+        player2.combatSceneIndex = -1;
+
+        if (!loser.isEnemy && loser.isInDeathsRow)
+            sceneManager.ChangeGamePhase(GameplayTest.GamePhase.EndGame);
+        else
+        {
+            if (loser.heldPoints < 0)
+                loser.isInDeathsRow = true;
+            sceneManager.ChangeGamePhase(GameplayTest.GamePhase.EndTurn);
+        }
+
+        //audioSource.PlayOneShot(explosionSFX, 2f);
+            
+        // Deletes the current combat scene.
+        sceneManager.UnloadCombatScene(SceneManager.GetSceneAt(combatSceneIndex), combatSceneIndex);
+
+        // Re-enable scene
+        sceneManager.EnableScene(0);
+
+        // Update player scores.
+        sceneManager.overworldScene.m_UpdatePlayerScore.RaiseEvent(player1.id);
+        if (!player2.isEnemy) {
+            sceneManager.overworldScene.m_UpdatePlayerScore.RaiseEvent(player2.id);
+        }
+        
+    }
+
+    private Action decideAttackAI()
+    {
+        int roll = Random.Range(0, 6); // 0 - 5
+        
+        if (roll < 3) // total 4/6 chance of using favorite attack
+        {
+            return player2.attackActions[player2.favoredAttack - 1];
+        }
+        else if (roll < 4) // Decide randomly 1/2 the time
+        {
+            return player2.attackActions[0];
+        }
+        else if (roll < 5)
+        {
+            return player2.attackActions[1];
+        }
+        else
+        {
+            return player2.attackActions[2];
+        }
+    }
+
+
+    // IENUMERATORS (Coroutines that delay code so animations can run)
+
+    // Allows waiting and then going back to code
+    public IEnumerator SelectionAnimation(float animationLength)
+    {
+        // Selection animation done within the previously called event
+
+        Debug.Log($"A player selected!");
+        yield return new WaitForSeconds(animationLength);
+        Debug.Log($"Done Selecting");
+        
+        if (onePlayerSelected) // Means both players decided
+>>>>>>> Stashed changes
         {
             return 1;
         }
@@ -686,6 +908,7 @@ public class CombatManager : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
 
 
     // Helper function to keep code from getting even more cluttered
@@ -700,4 +923,80 @@ public class CombatManager : MonoBehaviour
             return true;
         }
     }
+=======
+    public IEnumerator ShowChoiceAnimation(float animationTime)
+    {
+        
+        Debug.Log($"Revealing Actions!");
+        yield return new WaitForSeconds(animationTime);
+        Debug.Log($"Attack: {attackerAction.type}");
+        Debug.Log($"Defend: {defenderAction.type}");
+    
+        
+        DiceRolls(); // Calculates random damage and effects in the turn to get base damage / defense (also plays dice roll animation)
+    }
+
+    public IEnumerator DiceRollAnimation(float animationTime, int damageRoll, int defenseRoll)
+    {
+        //Play your dice rolling animation here (assuming they were started by the event)
+
+        yield return new WaitForSeconds(animationTime);
+        
+        Debug.Log($"Damage roll: {damageRoll}");
+        Debug.Log($"Defend roll: {defenseRoll}");
+
+        //m_DiceRolled.RaiseEvent(attacker, damageRoll); // Damage roll is the total of all rolled damage dice (if thats modified), and damage boosts, but no defense or type advantage involved.
+
+        //m_DiceRolled.RaiseEvent(defender, defenseRoll);
+        
+        PlayOutPhase(damageRoll, defenseRoll); // Now that we know the base damage, we can simply progress with combat after counting type advantage.
+
+    }
+
+    public IEnumerator AttackAndDefendAnimation(float animationTime, int damageToDeal)
+    {
+        // It was mentioned some stuff like the defense animation could be tuned in the animation timeline, but this
+        // moment of the method represents the start of the attack animation.
+
+        yield return new WaitForSeconds(animationTime);
+        
+        DealDamage(damageToDeal);
+
+    }
+
+    
+    public IEnumerator PhaseEndDelay(float animationTime) // Exists so there's a half a second or something before the phase ends
+    {
+        // It was mentioned some stuff like the defense animation could be tuned in the animation timeline, but this
+        // moment of the method represents the start of the attack animation.
+
+        yield return new WaitForSeconds(animationTime);
+        
+        endPhase();
+    }
+
+    public IEnumerator PauseCombatDelay(float animationTime) // Exists so there's a half a second or something before the phase ends
+    {
+        // It was mentioned some stuff like the defense animation could be tuned in the animation timeline, but this
+        // moment of the method represents the start of the attack animation.
+
+        yield return new WaitForSeconds(animationTime);
+        
+        pauseCombat();
+
+    }
+
+    public IEnumerator EndCombatDelay(float animationTime) // Exists so there's a half a second or something before the phase ends
+    {
+        // It was mentioned some stuff like the defense animation could be tuned in the animation timeline, but this
+        // moment of the method represents the start of the attack animation.
+
+        yield return new WaitForSeconds(animationTime);
+        
+        endCombat();
+
+    }
+
+
+>>>>>>> Stashed changes
 }
